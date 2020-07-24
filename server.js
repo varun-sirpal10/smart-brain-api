77987e5434562e2883ba8489e8d9cdd09a7aad27
Varun Sirpal <varunsirpal5@gmail.com>
2020-07-24 14:38:18 +0530

    updating DB

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-reticulated-23629',
       user : 'postgres',
       password : '2612',
       database : 'smartbrain'
